652f4dbaef25c91b42bdbf2a033460aa475dca31
janalois <dead_or_@live.se>
2023-05-16 22:56:04 +0200

    Made Stockholm static and added 30 minutes timer to refresh weather data

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const apiKey = "dc75337ee5abd0af183221a7c1e91f33";
 
-const searchBox = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
@@ -41,17 +39,12 @@ async function checkWeather(city) {
     document.querySelector(".weather").style.display = "block";
   }
 }
+var intervalId = window.setInterval(function () {
+  checkWeather("stockholm");
+}, 1800000);
 
-searchBtn.addEventListener("click", () => {
-  checkWeather(searchBox.value);
-});
+checkWeather("stockholm");
 
-searchBox.addEventListener("keypress", function (e) {
-  if (e.key == "Enter") {
-    checkWeather(searchBox.value);
-  }
-});
-//checkWeather();
 function refreshTime() {
   const timeDisplay = document.getElementById("time");
   const dateString = new Date().toLocaleString();
